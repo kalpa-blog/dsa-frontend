785ec549679ce4d4b22ef7836d8ee77ead298355
kalpa-blog <kalpablog@gmail.com>
2022-07-10 09:50:17 +0530

    Added initial files

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+       Hello world, this is a test page to build a CI/CD pipeline
+    </div>
+  );
+}
+
+export default App;
